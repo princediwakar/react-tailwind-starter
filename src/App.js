c488b82bb2ddbd11350415649f9c7000f1076b18
platonian <prince@indshine.com>
2020-07-05 18:45:56 +0530

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Logo from './logo.svg'
+
 
 export const App = () => (
-    <div className="App">
-      <h1 className="text-4xl">Welcome to your App</h1>
+    <div className="mt-10">
+      <img className="w-8 h-8 mx-auto" src={Logo} alt="Logo"/>
+      <h1 className="text-4xl text-center">Welcome to your App</h1>
     </div>
 )
 
